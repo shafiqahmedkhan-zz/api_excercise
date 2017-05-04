5e1c1d4f9fba8df1394bbb0dcbabb293d0e56c43
Shafiq Ahmed Khan <shafiqahmedkhan@Shafiqs-MacBook-Pro.local>
2017-05-04 21:50:23 +0100

    added getting user id feature

--- features/step_definitions/user_id.rb
+++ features/step_definitions/user_id.rb
@@ -1,14 +1,11 @@
 require_relative '../url_library'
-require 'test/unit/assertions'
-require 'test/unit'
+include Test::Unit::Assertions
 
 Given(/^I request a random user$/) do
   url = URL_Library.new
   @response = RestClient.get url.base_url + '/users'
   data = JSON.parse(@response.body)
-  puts  data[0]['id']
   @user_id = data[0]['id']
-  #assert_equal(1, data['messages'][0]['message']['id'])
 end
 
 Then(/^I get a (\d+) status code$/) do |arg|
@@ -16,9 +13,11 @@ Then(/^I get a (\d+) status code$/) do |arg|
 end
 
 Then(/^the address is received$/) do
-  pending
+  data = JSON.parse(@response.body)
+  assert_not_nil(data[0]['address'])
 end
 
 And(/^the email format is correct$/) do
-  pending
+  data = JSON.parse(@response.body)
+  data[0]['email'] =~ /\w+@\w+\.{1}[a-zA-Z]{2,}/
 end

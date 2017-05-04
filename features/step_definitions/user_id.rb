9d6e509fb1e758cd2940dd09357fe4c4acaf8adf
Shafiq Ahmed Khan <shafiqahmedkhan@Shafiqs-MacBook-Pro.local>
2017-05-05 00:06:49 +0100

    added retrieving posts scenario

--- features/step_definitions/user_id.rb
+++ features/step_definitions/user_id.rb
@@ -2,8 +2,8 @@ require_relative '../url_library'
 include Test::Unit::Assertions
 
 Given(/^I request a random user$/) do
-  url = URL_Library.new
-  @response = RestClient.get url.base_url + '/users'
+  @url = URL_Library.new
+  @response = RestClient.get @url.base_url + '/users'
   data = JSON.parse(@response.body)
   @user_id = data[0]['id']
 end
@@ -12,12 +12,37 @@ Then(/^I get a (\d+) status code$/) do |arg|
   assert_equal(200, @response.code)
 end
 
-Then(/^the address is received$/) do
+Then(/^response has an address$/) do
   data = JSON.parse(@response.body)
   assert_not_nil(data[0]['address'])
 end
 
-And(/^the email format is correct$/) do
+And(/^response has an email in the correct format$/) do
   data = JSON.parse(@response.body)
   data[0]['email'] =~ /\w+@\w+\.{1}[a-zA-Z]{2,}/
 end
+
+When(/^I retrieve posts$/) do
+  @valid_id = @user_id.to_s
+  @response = RestClient.get @url.base_url + '/users/' + @valid_id
+  @response = RestClient.get @url.base_url + '/posts/' + @valid_id
+end
+
+And(/^response has a valid id$/) do
+  step 'I retrieve posts'
+  data = JSON.parse(@response.body)
+  assert_not_nil(data['userId'])
+end
+
+And(/^response has a valid title$/) do
+  step 'I retrieve posts'
+  data = JSON.parse(@response.body)
+  assert_not_nil(data['title'])
+end
+
+And(/^response has a valid body$/) do
+  step 'I retrieve posts'
+  data = JSON.parse(@response.body)
+  assert_not_nil(data['body'])
+end
+
--- features/user_id.feature
+++ features/user_id.feature
@@ -1,7 +1,15 @@
 Feature:
 
-  Scenario: Getting a user_id and printing out the address and  email
+  Scenario: Printing out the address and email
     Given I request a random user
     Then I get a 200 status code
-    And the address is received
-    And the email format is correct
+    And response has an address
+    And response has an email in the correct format
+
+  Scenario: Retrieving posts
+    Given I request a random user
+    Then I get a 200 status code
+    When I retrieve posts
+    Then response has a valid id
+    And response has a valid title
+    And response has a valid body
